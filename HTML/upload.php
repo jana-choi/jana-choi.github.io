da64a6d1d02438ebc3cc70dba686fa95853fff87
jana-choi <lovej0918@gmail.com>
2020-01-17 23:18:09 +0900

    iframe test

--- HTML/upload.php
+++ HTML/upload.php
@@ -3,14 +3,15 @@ $source = $_FILES['profile']['tmp_name'];
 $dest = "./" .basename($_FILES['profile']['name']);
 move_uploaded_file($source, $dest);
 ?>
-
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>upload php</title>
     </head>
     <body>
+        <p><h3><a href="index.html">Go To Index</a></h3></p>
         <img src="<?=$_FILES['profile']['name']?>" alt=""/>
     </body>
 </html>
